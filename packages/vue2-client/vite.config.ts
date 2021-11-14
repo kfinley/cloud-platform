e03657db6b374dee35aa1bf189dfc3a838fabd63
Kyle Finley <Kyle@KyleFinley.net>
2021-11-14 00:13:54 +0000

    Vite: vue2-client app running in container using vite

---
+++ packages/vue2-client/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+//TODO: Look back into this and figure out why it isn't working...
+// import { ViteAliases } from 'vite-aliases'
+// import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    createVuePlugin(),
+    // ViteAliases()
+  ],
+  server: {
+    port: 8080
+  },
+  // resolve: {
+  //   //TODO: This doesn't seem to be working....
+  //   alias: [
+  //     {
+  //       find: '@',
+  //       replacement: path.resolve(__dirname, 'src')
+  //     }
+  //   ]
+  // },
+  css: {
+    preprocessorOptions: {
+    //   scss: {
+    //     additionalData: `@import "@platform8/web-ui/src/styles/_variables.scss";`
+    //   }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 600,
+    cssCodeSplit: false
+  },
+  define: {
+    'process.env': process.env
+  }
+});
